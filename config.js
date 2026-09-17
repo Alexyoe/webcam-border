e014f5f50eb4d35cd2cd4df6d5c72f39b196ffcf
Alex Yoesting <alexyoe@gmail.com>
2026-09-17 13:14:22 -0400

    Update to work with new effects

---
+++ config.js
@@ -0,0 +1,36 @@
+window.OVERLAY_CONFIG = {
+	width: 1920,
+	height: 1080,
+
+	// "rainbow" or "custom"
+	mode: 'main',
+
+	// Used when mode is "custom"
+	firstColor: '#fffd00',
+	secondColor: '#ffc400',
+	thirdColor: '#880000',
+
+	// Speed of rotation
+	durationSeconds: 15,
+
+	// Other effects
+	effect: 'snow',
+
+	snow: {
+		flakes: 200,
+		minSize: 1,
+		maxSize: 8,
+		minSpeed: 0.5,
+		maxSpeed: 3,
+		drift: 0.8
+	},
+
+	leaves: {
+		count: 60,
+		minSize: 8,
+		maxSize: 22,
+		minSpeed: 0.5,
+		maxSpeed: 2,
+		colors: ['#d35400', '#e67e22', '#f39c12', '#c0392b', '#8e5a2b', '#b8860b']
+	}
+}
